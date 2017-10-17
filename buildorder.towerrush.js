1d79d103b6a31aa51cae0b6e15aac79c22c6377a
Doji <stijn.c.dejongh@gmail.com>
2017-10-17 15:24:13 +0200

    More logic issues fixed

--- buildorder.towerrush.js
+++ buildorder.towerrush.js
@@ -1,6 +1,6 @@
 var spawning = require('structs.spawningpool');
 var towerRange = 7;
-var creepAmounts = [4,10,4];
+var creepAmounts = [6,2,5];
 
 var towerrush = {
     run: function() {
@@ -12,38 +12,33 @@ var towerrush = {
         }
         if(creep) {
             var controller = creep.room.controller;
-            if(controller.level < 2) {
+            if(controller.level < 8) {
                 creep.upgradeController(controller);
             }
             
             var room = creep.room
             if(room.find(FIND_MY_CONSTRUCTION_SITES).length < 2) {
-                    console.log('TOWERRUSHING!');
-                    var enemySpawn = room.find(Game.FIND_HOSTILE_SPAWNS);
+                    var enemySpawn = room.find(Game.FIND_HOSTILE_STRUCTURES);
                     if(enemySpawn.length > 0) {
+                        console.log('TOWERRUSHING!');
                         var enemyPosition = enemySpawn[0].pos;
                         var inRange = room.lookForAtArea(LOOK_TERRAIN, enemyPosition.y + towerRange, enemyPosition.x - towerRange, enemyPosition.y - towerRange, enemyPosition.x + towerRange);
                         if(room.createConstructionSite(inRange[0].pos, STRUCTURE_TOWER) != OK) {
                                 console.log('Error while rushing!');
                             } else {
-                                creepAmounts = [6,2,10];
+                                creepAmounts = [10,2,5];
                             }
                         
                     } else {
-                        console.log('Defencing!');
+                        
                         var spawningPools = Game.spawns;
                         for (var poolName in spawningPools) {
+                            console.log('Defencing!');
                             var pool = spawningPools[poolName];
-                            if(room.createConstructionSite(pool.pos.x - 2, pool.pos.y, STRUCTURE_TOWER) != OK) {
-                                console.log('Error while defencing!');
-                            } else {
-                                if(room.createConstructionSite(pool.pos.x + 2, pool.pos.y, STRUCTURE_TOWER) != OK) {
-                                    console.log('Error while defencing!');
-                                } else {
-                                    creepAmounts = [6,2,10];
-                                }
-                            }
+                            room.createConstructionSite(pool.pos.x - 2, pool.pos.y, STRUCTURE_TOWER);
+                            room.createConstructionSite(pool.pos.x + 2, pool.pos.y, STRUCTURE_TOWER);
                         }
+                        
                     }
                     
             }
